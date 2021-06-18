627f47268d4d772c0ce6b55c69c12db9c8ff0e07
Flor-AC <florac206@gmail.com>
2021-06-18 01:11:38 -0500

    se agregaron lineas de cogido

--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const PORT = process.env.PORT || 5000
 
 const mongoose = require('mongoose');
@@ -10,6 +11,7 @@ mongoose.Promise = global.Promise;
 
 app.use(express.static('public'));
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', require('./routes/crafts.router.js'));
 app.use(function(err,req,res,next){
